0ec1ef3c06c8e5c3128ef687018d8e498fb6ea9f
OMEN <grigorenkoeduard6@gmail.com>
2022-11-04 13:35:44 +0100

    Initial commit

---
+++ src/china.java
@@ -0,0 +1,13 @@
+public class china {
+    public static void main(String[] args) {
+        long china =1_411_778_724;
+        int countries =197;
+        System.out.println(china * countries);
+
+        System.out.println("_____________");
+
+        long felix = 3_011_456_567L;
+        System.out.println(felix / 60 / 60 /24 / 365);
+
+    }
+}
